3f0cf95083f7eeef9c4724d087f8ca1a0fd9f9af
bloome-alex <alex99valdiviezo@gmail.com>
2023-02-10 11:24:58 -0300

    multiple cors

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 
 
 const app = express()
 
-app.use(cors())
+app.use((req, res, next) => {
+    const allowedOrigins = ['https://rueddo-api-production.up.railway.app/', 'https://rueddo-vuelder-production.up.railway.app', 'https://rueddo-mobile-production.up.railway.app'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.use(express.static('public'))
 
